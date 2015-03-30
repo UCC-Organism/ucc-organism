988ea3effc6b7a03a839ef15770d1f68d3398054
Marcin Ignac <marcin.ignac@gmail.com>
2015-03-30 20:04:36 +0100

    Fixed agent texturing bug

--- src/ucc/sys/agentPointSpriteUpdaterSys.js
+++ src/ucc/sys/agentPointSpriteUpdaterSys.js
@@ -19,9 +19,15 @@ function agentPointSpriteUpdaterSys(state) {
   if (!state.pointSpriteMeshEntity) {
     var image = Platform.isPlask ? __dirname + '/../../../assets/agents_5.png' : 'assets/agents_5.png';
     var pointSpriteGeometry = new Geometry({ vertices: true, colors: true, normals: true, texCoords: true });
-    var pointSpriteMaterial = new PointSpriteTextured({ pointSize: 30 * state.DPI, texture: Texture2D.load(image, { flip: false }), texSize: new Vec2(1/10, 1/15), texOffset: new Vec2(1/10, 1/10) });
+    var pointSpriteMaterial = new PointSpriteTextured({
+      pointSize: 30 * state.DPI,
+      texture: Texture2D.load(image, { flip: false }),
+      texSize: new Vec2(1/10, 1/15),
+      texOffset: new Vec2(1/10, 1/15)
+    });
     state.pointSpriteMeshEntity = {
-      agentMesh: true, mesh: new Mesh(pointSpriteGeometry, pointSpriteMaterial, { points: true } )
+      agentMesh: true,
+      mesh: new Mesh(pointSpriteGeometry, pointSpriteMaterial, { points: true } )
     }
     state.entities.push(state.pointSpriteMeshEntity);
   }
