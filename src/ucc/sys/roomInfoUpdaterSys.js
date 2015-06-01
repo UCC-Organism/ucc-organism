79caa5c89b03c3f782903fa71b706112015e99c5
Marcin Ignac <marcin.ignac@gmail.com>
2015-06-01 22:34:10 +0100

    Updating room labels only if visible

--- src/ucc/sys/roomInfoUpdaterSys.js
+++ src/ucc/sys/roomInfoUpdaterSys.js
@@ -18,14 +18,16 @@ function roomInfoUpdaterSys(state) {
     }
   })
 
-  state.map.rooms.forEach(function(room) {
-   var roomNode = R.find(R.where({roomId: room.id}), state.map.selectedNodes);
-   if (roomNode) {
-      var name = room.id;
-      if (name.indexOf('room') == 0) name = room.type;
-      state.debugText.drawText(name + ' / ' + room.agentCount, roomNode.position);
-   }
- })
+  if (state.showLabels) {
+    state.map.rooms.forEach(function(room) {
+     var roomNode = R.find(R.where({roomId: room.id}), state.map.selectedNodes);
+     if (roomNode) {
+        var name = room.id;
+        if (name.indexOf('room') == 0) name = room.type;
+        state.debugText.drawText(name + ' / ' + room.agentCount, roomNode.position);
+     }
+   })
+  }
 }
 
 module.exports = roomInfoUpdaterSys;
