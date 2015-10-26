f2b4be5b1332ce0461703d6aa0998e3a09008a13
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-10-26 12:59:34 +0100

    bugfix: side-effect-free comparison statement was probably intended to be assignment, as other parts of the code seems to depend on theses assignments

--- src/ucc/data/Client.js
+++ src/ucc/data/Client.js
@@ -75,11 +75,11 @@ Client.prototype.updateCurrentState = function() {
         id: agentId
       }
       if (agentId.match(/^student/)) { agent.type = 'student'; };
-      if (agentId.match(/^teacher/)) { agent.type = 'teacher'; agent.programme == 'Teacher'; }
-      if (agentId.match(/^researcher/)) { agent.type = 'researcher'; agent.programme == 'Researcher'; }
-      if (agentId.match(/^janitor/)) { agent.type = 'janitor'; agent.programme == 'Janitor'; }
-      if (agentId.match(/^cook/)) { agent.type = 'cook'; agent.programme == 'Cook'; }
-      if (agentId.match(/^admin/)) { agent.type = 'admin'; agent.programme == 'Admin'; }
+      if (agentId.match(/^teacher/)) { agent.type = 'teacher'; agent.programme = 'Teacher'; }
+      if (agentId.match(/^researcher/)) { agent.type = 'researcher'; agent.programme = 'Researcher'; }
+      if (agentId.match(/^janitor/)) { agent.type = 'janitor'; agent.programme = 'Janitor'; }
+      if (agentId.match(/^cook/)) { agent.type = 'cook'; agent.programme = 'Cook'; }
+      if (agentId.match(/^admin/)) { agent.type = 'admin'; agent.programme = 'Admin'; }
 
       if (agentState.description == 'away') {
         agent.targetMode = AgentModes.Away;
